b36d30b8a26105be8a6bc94be007e588699bd726
beiyue99 <2377142053@qq.com>
2024-05-10 00:41:05 +0800

    添加配置模块

---
+++ demo0.4/Client.go
@@ -0,0 +1,36 @@
+package main
+
+import (
+	"fmt"
+	"net"
+	"time"
+)
+
+// 模拟客户端
+
+func main() {
+	fmt.Println("client start...")
+	time.Sleep(time.Second)
+	conn, err := net.Dial("tcp", "127.0.0.1:8888")
+	if err != nil {
+		fmt.Println("client start err,exit!")
+		return
+	}
+
+	for {
+		//调用write写数据
+		_, err := conn.Write([]byte("hello zinxV0.4..."))
+		if err != nil {
+			fmt.Println("conn Write err", err)
+			return
+		}
+		buf := make([]byte, 512)
+		cnt, err := conn.Read(buf)
+		if err != nil {
+			fmt.Println("Read buf err")
+			return
+		}
+		fmt.Printf("server callback %s,cnt=%d\n", buf, cnt)
+		time.Sleep(time.Second)
+	}
+}
---
+++ demo0.4/go.mod
@@ -0,0 +1,7 @@
+module zinxV0.1
+
+go 1.22.2
+
+replace zinx => ../zinx
+
+require zinx v0.0.0-00010101000000-000000000000
